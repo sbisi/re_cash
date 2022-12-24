585edbbccd5b260364d1db42c9c3d2927ad122cd
sbisi <62792590+sbisi@users.noreply.github.com>
2022-12-24 09:43:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import os
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+# import dash_core_components as dcc
+from dash import dcc
+# import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output
 from tqdm import tqdm
 
